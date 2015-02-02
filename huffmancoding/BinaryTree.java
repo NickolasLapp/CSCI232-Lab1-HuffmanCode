16845332aae8d71b3d370391be5169d60245bfaa
Nickolas Lapp <nickolaslapp@gmail.com>
2015-02-02 06:48:18 -0700

    Auto Formatter Change

--- huffmancoding/BinaryTree.java
+++ huffmancoding/BinaryTree.java
@@ -3,125 +3,124 @@ package huffmancoding;
 import java.io.*;
 import java.util.*; // for Stack class
 
-class Node implements Comparable
-{
-	public int iData; // data item (key)
-	public Byte dData; // data item
-	public Node leftChild; // this node's left child
-	public Node rightChild; // this node's right child
-	
-	public Node(int iData, byte dData)
-	{
-		this.iData = iData;
-		this.dData = dData;
-	}
-	
-	public Node(Node childOne, Node childTwo)
-	{
-		this.iData = childOne.iData + childTwo.iData;
-		this.dData = null;
-		
-		if(childOne.compareTo(childTwo) == 1)
-		{
-			leftChild = childOne;
-			rightChild = childTwo;
-		}
-		else
-		{
-			leftChild = childTwo;
-			rightChild = childOne;
-		}
-		
-	}
-	
-	
-	public void displayNode() // display ourself
-	{
-		System.out.print('{');
-		System.out.print(iData);
-		System.out.print(", ");
-		System.out.print(dData);
-		System.out.print("} ");
-	}
-	@Override
-	public int compareTo(Object compareTo) {
-		if( iData < ((Node)compareTo).iData)
-			return -1;
-		else
-			return 1;
-	}
+class Node implements Comparable<Node> {
+    public int iData; // data item (key)
+    public Byte dData; // data item
+    public Node leftChild; // this node's left child
+    public Node rightChild; // this node's right child
 
-	public void printDataAndCodes(String code) {
-		if(dData != null)
-			System.out.println("dData: " + dData + "\tCode: " + code + "\tFrequency: " + iData);
-		else
-		{
-			leftChild.printDataAndCodes(code + "0");
-			rightChild.printDataAndCodes(code + "1");
-		}
-	}
+    public Node(int iData, byte dData) {
+        this.iData = iData;
+        this.dData = dData;
+    }
 
-	public void fillEncodingTable(Map<Byte, byte[]> encodingTable, byte[] code) {
-		if(dData != null)
-			encodingTable.put(dData, code);
-		else
-		{
-			byte nextCodeLeft [] = Arrays.copyOf(code, code.length+1);
-			nextCodeLeft[nextCodeLeft.length-1] = (byte)0;
-			leftChild.fillEncodingTable(encodingTable, nextCodeLeft);
-			
-			byte nextCodeRight [] = Arrays.copyOf(code, code.length+1); 
-			nextCodeRight[nextCodeRight.length-1] = (byte)1;
-			rightChild.fillEncodingTable(encodingTable, nextCodeRight);
-		}
-	}
+    public Node(Node childOne, Node childTwo) {
+        this.iData = childOne.iData + childTwo.iData;
+        this.dData = null;
 
-	public String readCodeAsChar(String code) {
-		Node head = this;
-		Node current = this;
-		String message = "";
-		
-		for(char c : code.toCharArray())
-		{
-			if(current.dData != null)
-			{
-				Character coded = (char)(current.dData & 0xFF);
-				message = message.concat(coded.toString());
-				current = head;
-			}
-			if('1' == c)
-				current = current.rightChild;
-			else
-				current = current.leftChild;
-		}		
-		return message;
-	}
-	
-	public byte[] toByteArray(List<Byte> in) {
-	    final int n = in.size();
-	    byte ret[] = new byte[n];
-	    for (int i = 0; i < n; i++) {
-	        ret[i] = in.get(i);
-	    }
-	    return ret;
-	}
-	
-	public byte[] readCodeAsBinary(byte[] code, int numBytes) {
-		Node head = this;
-		Node current = this;
-		ArrayList<Byte> message = new ArrayList<Byte>(1000);
-		for(int i = 0; i < numBytes; i++)
-		{
-			if(current.dData != null)
-			{
-				message.add(current.dData);
-				current = head;
-			}
-			if((byte)1 == code[i])
-				current = current.rightChild;
-			else
-				current = current.leftChild;
-		}
-		return toByteArray(message);
-	}
+        if (childOne.compareTo(childTwo) == 1) {
+            leftChild = childOne;
+            rightChild = childTwo;
+        } else {
+            leftChild = childTwo;
+            rightChild = childOne;
+        }
+
+    }
+
+    public void displayNode() // display ourself
+    {
+        System.out.print('{');
+        System.out.print(iData);
+        System.out.print(", ");
+        System.out.print(dData);
+        System.out.print("} ");
+    }
+
+    @Override
+    public int compareTo(Node compareTo) {
+        if (iData < compareTo.iData)
+            return -1;
+        else
+            return 1;
+    }
+
+    public void printDataAndCodes(String code) {
+        if (dData != null)
+            System.out.println("dData: " + dData + "\tCode: " + code
+                    + "\tFrequency: " + iData);
+        else {
+            leftChild.printDataAndCodes(code + "0");
+            rightChild.printDataAndCodes(code + "1");
+        }
+    }
+
+    public void fillEncodingTable(Map<Byte, byte[]> encodingTable, byte[] code) {
+        if (dData != null)
+            encodingTable.put(dData, code);
+        else {
+            byte nextCodeLeft[] = Arrays.copyOf(code, code.length + 1);
+            nextCodeLeft[nextCodeLeft.length - 1] = (byte) 0;
+            leftChild.fillEncodingTable(encodingTable, nextCodeLeft);
+
+            byte nextCodeRight[] = Arrays.copyOf(code, code.length + 1);
+            nextCodeRight[nextCodeRight.length - 1] = (byte) 1;
+            rightChild.fillEncodingTable(encodingTable, nextCodeRight);
+        }
+    }
+
+    public String readCodeAsChar(String code) {
+        Node head = this;
+        Node current = this;
+        String message = "";
+
+        for (char c : code.toCharArray()) {
+            if (current.dData != null) {
+                Character coded = (char) (current.dData & 0xFF);
+                message = message.concat(coded.toString());
+                current = head;
+            }
+            if ('1' == c)
+                current = current.rightChild;
+            else
+                current = current.leftChild;
+        }
+        return message;
+    }
+
+    public byte[] toByteArray(List<Byte> in) {
+        final int n = in.size();
+        byte ret[] = new byte[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = in.get(i);
+        }
+        return ret;
+    }
+
+    public byte[] readCodeAsBinary(byte[] code, int numBytes) {
+        Node head = this;
+        Node current = this;
+        ArrayList<Byte> message = new ArrayList<Byte>(1000);
+        for (int i = 0; i < numBytes; i++) {
+            if (current.dData != null) {
+                message.add(current.dData);
+                current = head;
+            }
+            if ((byte) 1 == code[i])
+                current = current.rightChild;
+            else
+                current = current.leftChild;
+        }
+        return toByteArray(message);
+    }
+
+    public void printDataAndCodesAsChars(String code) {
+        if (dData != null)
+            System.out.println("dData: " + (char) (dData & 0xff) + "\tCode: "
+                    + code + "\tFrequency: " + iData);
+        else {
+            leftChild.printDataAndCodesAsChars(code + "0");
+            rightChild.printDataAndCodesAsChars(code + "1");
+        }
+    }
 }
--- huffmancoding/HuffmanTree.java
+++ huffmancoding/HuffmanTree.java
@@ -13,126 +13,93 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class HuffmanTree {
-	
-	private Map<Byte, Integer> frequencyTable; 
-	private Map<Byte, byte[]> encodingTable; 
-	private Node encodingTree;
-	
-	public HuffmanTree(InputStream dataStream) throws IOException
-	{
-		generateFrequencyTableFromBytes(dataStream);
-		buildTree();
-	}
-	
-	private void generateFrequencyTable(InputStream dataStream) throws IOException
-	{	
-		byte buffer[] = new byte[4000000];
-		frequencyTable = new HashMap<Byte, Integer>();
-		
-		dataStream.read(buffer);
-				
-		for(byte b : buffer)
-		{
-			if((char)(b & 0xFF) == '$')
-				break;
-			Integer previousFrequency = frequencyTable.get(b);
-			frequencyTable.put(b, previousFrequency == null ? 1 : previousFrequency + 1);
-		}
-	}
-	
-	private void generateFrequencyTableFromBytes(InputStream dataStream) throws IOException
-	{	
-		byte buffer[] = new byte[4000000];
-		frequencyTable = new HashMap<Byte, Integer>();
-		
-		int bytesRead = dataStream.read(buffer);
-				
-		for(int i = 0; i < bytesRead; i++)
-		{
-			Integer previousFrequency = frequencyTable.get(buffer[i]);
-			frequencyTable.put(buffer[i], previousFrequency == null ? 1 : previousFrequency + 1);
-		}
-	}
-	
-	private void buildTree()
-	{
-		PriorityQueue<Node> nodes = new PriorityQueue<Node>(frequencyTable.size());
-		
-		for(byte b : frequencyTable.keySet())
-		{
-			nodes.add(new Node(frequencyTable.get(b), b));
-		}
-		
-		while(nodes.size() > 1)
-		{
-			nodes.add(new Node(nodes.poll(), nodes.poll()));
-		}
-		encodingTree = nodes.poll();	
-		encodingTable = new HashMap<Byte, byte[]>();
-		encodingTree.fillEncodingTable(encodingTable, new byte[0]);
-	}
 
-	private byte[] encodeAll(InputStream dataStream) throws IOException
-	{
-		byte buffer []= new byte[4000000];
-		
-		byte codedMessage []= new byte[40000000];
-		int bytesRead = dataStream.read(buffer);
-		int codeIndex = 0;
-		
-		
-		
-		for(int i = 0; i < bytesRead; i++)
-		{
-			byte[] code = encodingTable.get(buffer[i]);
-			if(code != null)
-			{
-				for(byte b : code)
-					codedMessage[codeIndex++] = b;
-			}
-			else
-				break;
-		}
+    private Map<Byte, Integer> frequencyTable;
+    private Map<Byte, byte[]> encodingTable;
+    private Node encodingTree;
 
-		return Arrays.copyOf(codedMessage, codeIndex);
-	}
-	
-	private byte[] decodeBytes(InputStream dataStream) throws IOException
-	{		
-		byte codedBytes[] = new byte [4000000];
-		int bytesRead = dataStream.read(codedBytes);
-	    
-	    return encodingTree.readCodeAsBinary(codedBytes, bytesRead);
-	}
-	
-	private String decodeChars(InputStream data) {
-	    java.util.Scanner s = new java.util.Scanner(data).useDelimiter("\\A");
-	    String codedString = s.hasNext() ? s.next() : "";
-	    s.close();
-	    
-	    return encodingTree.readCodeAsChar(codedString);
-	}
-	
-	public static void main(String[] args) throws IOException
-	{
-		byte buffer[] = new byte[4000000];
-		InputStream data = new FileInputStream("/home/nickolas/Desktop/testFile.txt");
-		data.read(buffer);	
-		
-		data = new FileInputStream("/home/nickolas/Desktop/testFile.txt");
-		HuffmanTree testTree = new HuffmanTree(data);
-		data =  new FileInputStream("/home/nickolas/Desktop/testFile.txt");
-		
-		byte[] encodedBytes = testTree.encodeAll(data);
-		testTree.encodingTree.printDataAndCodes("");
-		
-		data = new ByteArrayInputStream(encodedBytes);
+    public HuffmanTree(InputStream dataStream) throws IOException {
+        generateFrequencyTableFromBytes(dataStream);
+        buildTree();
+    }
 
-		byte[] dataToWrite = testTree.decodeBytes(data);
-		FileOutputStream out = new FileOutputStream("/home/nickolas/Desktop/outputtestFile.txt");
-		
-		for(byte b:dataToWrite)
-			out.write(b);
-		out.close();
-	}
+    private void generateFrequencyTableFromBytes(InputStream dataStream) throws IOException {
+        byte buffer[] = new byte[4000000];
+        frequencyTable = new HashMap<Byte, Integer>();
+
+        int bytesRead = dataStream.read(buffer);
+
+        for (int i = 0; i < bytesRead; i++) {
+            Integer previousFrequency = frequencyTable.get(buffer[i]);
+            frequencyTable.put(buffer[i], previousFrequency == null ? 1 : previousFrequency + 1);
+        }
+    }
+
+    private void buildTree() {
+        PriorityQueue<Node> nodes = new PriorityQueue<Node>(frequencyTable.size());
+
+        for (byte b : frequencyTable.keySet()) {
+            nodes.add(new Node(frequencyTable.get(b), b));
+        }
+
+        while (nodes.size() > 1) {
+            nodes.add(new Node(nodes.poll(), nodes.poll()));
+        }
+        encodingTree = nodes.poll();
+        encodingTable = new HashMap<Byte, byte[]>();
+        encodingTree.fillEncodingTable(encodingTable, new byte[0]);
+    }
+
+    private byte[] encodeAll(InputStream dataStream) throws IOException {
+        byte buffer[] = new byte[4000000];
+
+        byte codedMessage[] = new byte[40000000];
+        int bytesRead = dataStream.read(buffer);
+        int codeIndex = 0;
+
+        for (int i = 0; i < bytesRead; i++) {
+            byte[] code = encodingTable.get(buffer[i]);
+            if (code != null) {
+                for (byte b : code)
+                    codedMessage[codeIndex++] = b;
+            } else
+                break;
+        }
+
+        return Arrays.copyOf(codedMessage, codeIndex);
+    }
+
+    private byte[] decodeBytes(InputStream dataStream) throws IOException {
+        byte codedBytes[] = new byte[4000000];
+        int bytesRead = dataStream.read(codedBytes);
+
+        return encodingTree.readCodeAsBinary(codedBytes, bytesRead);
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte buffer[] = new byte[4000000];
+        InputStream data = new FileInputStream("/home/nickolas/Desktop/testFile.txt");
+        data.read(buffer);
+
+        data = new FileInputStream("/home/nickolas/Desktop/testFile.txt");
+        HuffmanTree testTree = new HuffmanTree(data);
+        data = new FileInputStream("/home/nickolas/Desktop/testFile.txt");
+
+        byte[] encodedBytes = testTree.encodeAll(data);
+        testTree.encodingTree.printDataAndCodes("");
+        testTree.encodingTree.printDataAndCodesAsChars("");
+
+        FileOutputStream out = new FileOutputStream("/home/nickolas/Desktop/outputEncodedFile");
+        for (byte b : encodedBytes)
+            out.write(b);
+        out.close();
+
+        data = new ByteArrayInputStream(encodedBytes);
+
+        byte[] dataToWrite = testTree.decodeBytes(data);
+        out = new FileOutputStream("/home/nickolas/Desktop/outputtestFile.txt");
+        for (byte b : dataToWrite)
+            out.write(b);
+        out.close();
+    }
 }
